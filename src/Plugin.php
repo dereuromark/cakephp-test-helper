f65c325413800bd584a6b1cd680a969eb5d1565f
Flavius <highstrike@gmail.com>
2018-07-20 16:15:33 +0300

    updated per review

--- src/Plugin.php
+++ src/Plugin.php
@@ -4,22 +4,9 @@ namespace TestHelper;
 
 use Cake\Core\BasePlugin;
 use Cake\Routing\RouteBuilder;
-use Cake\Routing\Route\DashedRoute;
 
-/**
- * Backend Plugin
- *
- * @author Flavius
- * @version 1.0
- */
 class Plugin extends BasePlugin
 {
-    /**
-     * Plugin name
-     * @var string
-     */
-    protected $name = 'TestHelper';
-
     /**
      * {@inheritDoc}
      * @see \Cake\Core\BasePlugin::routes()
@@ -31,8 +18,8 @@ class Plugin extends BasePlugin
             ['path' => '/test-helper'],
             function (RouteBuilder $routes) {
                 $routes->connect('/', ['controller' => 'TestHelper', 'action' => 'index']);
-
-                $routes->fallbacks(DashedRoute::class);
+				
+                $routes->fallbacks();
             }
         );
     }
