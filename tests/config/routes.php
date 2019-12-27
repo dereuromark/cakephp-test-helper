53c6c59adfdab108309e1d5813cea5c54202932c
mscherer <mark.scherer@spryker.com>
2019-12-27 23:40:07 +0100

    Remove unnecessary file

--- tests/config/routes.php
+++ tests/config/routes.php
@@ -1,3 +1,14 @@
 <?php
 
-require dirname(dirname(__DIR__)) . '/config/routes.php';
+use Cake\Routing\RouteBuilder;
+use Cake\Routing\Router;
+
+Router::plugin(
+	'TestHelper',
+	['path' => '/test-helper'],
+	function (RouteBuilder $routes) {
+		$routes->connect('/', ['controller' => 'TestHelper', 'action' => 'index']);
+
+		$routes->fallbacks();
+	}
+);
